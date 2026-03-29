b88c7c5dc7eff68179c697ea51e64f9812168673
Aman Sumesh <amansumesh2005@gmail.com>
2026-03-29 15:46:34 +0530

    Fix CORS issue

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://deep-shield-zeta.vercel.app",
+        "http://localhost:5173"
+    ],    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
